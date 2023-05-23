001be1f837f1a0181dbce2fb9d19ebb60f65f47f
Mallow <linlam0923@gmail.com>
2023-05-23 00:51:58 -0400

    save

--- database/index.js
+++ database/index.js
@@ -3,19 +3,27 @@ require('dotenv').config();
 
 
 const db = new Pool ({
-  host: process.env.DB_HOST,
-  // user: process.env.DB_USER,
-  // password: process.env.DB_PASS,
+  host: '18.219.111.8',
+  user: 'postgres',
+  password: '0000',
+  port: 5432,
   database: process.env.DB_NAME,
   })
 
-  db.on('error', (err, client) => {
-    console.error('Unexpected error on idle client', err)
-    process.exit(-1)
-  })
+  // db.on('error', (err, client) => {
+  //   console.error('Unexpected error on idle client', err)
+  //   process.exit(-1)
+  // })
 
-  db.on('connect', client => {
-    console.log('Connected to database: ', client.database)
+  db.connect((err) => {
+    if (err) {
+      console.log('err', err)
+    } else {
+      console.log('Connected to database: ')
+    }
   })
+  // db.on('connect', client => {
+  //   console.log('Connected to database: ', client.database)
+  // })
 
 module.exports = db;
