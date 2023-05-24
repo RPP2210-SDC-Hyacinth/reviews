a3b5f837dedcf6e30bdaadabd79f832f4a470208
Mallow <linlam0923@gmail.com>
2023-05-23 21:36:12 -0400

    make loader io route

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const controller = require('./controllers.js');
+const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -20,6 +21,25 @@ app.put('/reviews/:product_id/helpful', controller.markHelpful);
 app.put('/reviews/:product_id/report', controller.reportReview);
 
 app.put('/reviews/:product_id/delete', controller.deleteReview);
+
+app.get('loaderio-9960bc98b37a711c02be52e225a31290.txt', (req, res) => {
+  const options = {
+    root: path.join(__dirname)
+  };
+  const fileName = 'loaderio-9960bc98b37a711c02be52e225a31290.txt';
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+        console.log('err sending file', err);
+    } else {
+        console.log('Sent:', fileName);
+    }
+  });
+})
+
+
+
+
+
 
 const port = 6000;
 
