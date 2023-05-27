714b2c844df9bb0623ca4f946fb87c95dc8179e8
Mallow <linlam0923@gmail.com>
2023-05-26 23:13:55 -0400

    added redis cache

--- server/controllers.js
+++ server/controllers.js
@@ -6,48 +6,44 @@ const markHelpful = require('../database/queries/markHelpful.js');
 const reportReview = require('../database/queries/reportReview.js');
 const deleteReview = require('../database/queries/deleteReview.js');
 
-const { createClient } = require('redis');
-const redis = createClient();
-
-// (async () => {
-//   // redisClient = createClient();
-//   redis.on("error", (error) => console.error(`Error : ${error}`));
-//   await redis.connect();
-// })();
-
 exports.getReviews = async (req, res) => {
-
   var data = req.query;
 
-  const cache = await redis.get(`reviews/product_id=${data.product_id}`);
+  const cache = await redis.get(`product_id=${data.product_id} reviews`);
 
   if (cache) {
-    console.log('cache', cache)
+    res.status(200).send(JSON.parse(cache));
   } else {
-    // getReviews(data)
-    // .then(async (response) => {
-    //   await redis.set(`reviews/product_id=${data.product_id}`, JSON.stringify(response));
-    //   res.status(200).send(response);
-    // })
-    // .catch((err) => {
-    // res.status(404).send('error getReviews', err)
-    // })
-    console.log('no cache')
+    getReviews(data)
+    .then(async (response) => {
+      await redis.set(`product_id=${data.product_id} reviews`, JSON.stringify(response));
+      res.status(200).send(response);
+    })
+    .catch((err) => {
+    res.status(404).send('error getReviews', err)
+    })
   }
-
 };
 
-exports.getMetadata = function(req, res) {
+exports.getMetadata = async (req, res) => {
 
   var product_id = req.query.product_id;
 
-  getMetadata(product_id)
-  .then((response) => {
-    res.status(200).send(response);
-  })
-  .catch((err) => {
-      res.status(404).send('error getMetadata', err)
-  })
+  const cache = await redis.get(`product_id=${product_id} reviews Metadata`);
+
+  if (cache) {
+    res.status(200).send(JSON.parse(cache));
+  }  else {
+    getMetadata(product_id)
+    .then(async (response) => {
+      await redis.set(`product_id=${product_id} reviews Metadata`, JSON.stringify(response));
+      res.status(200).send(response);
+    })
+    .catch((err) => {
+        res.status(404).send('error getMetadata', err)
+    })
+  }
+
 }
 
 exports.postReview = function(req, res) {
